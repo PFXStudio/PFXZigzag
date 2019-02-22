d6978b634185650fda43d525482751b8cff1e281
succorer <succorer@hanwha.com>
2019-02-22 14:19:06 +0900

    update views

---
+++ PFXZigzag/PFXZigzag/Defines/DefineStrings.swift
@@ -0,0 +1,14 @@
+//
+//  DefineStrings.swift
+//  PFXZigzag
+//
+//  Created by succorer on 2019. 2. 22..
+//  Copyright © 2019년 PFXStudio. All rights reserved.
+//
+
+import UIKit
+
+class DefineStrings: NSObject {
+    static let kAgreedAutoLogin = "kAgreedAutoLogin"
+
+}
--- PFXZigzag/PFXZigzag/ViewController.swift
+++ PFXZigzag/PFXZigzag/ViewController.swift
@@ -7,20 +7,124 @@
 //
 
 import UIKit
+import SwiftProgressHUD
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var autoLoginLegalButton: NSLayoutConstraint!
     @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var statusLabel: UILabel!
+    @IBOutlet weak var detailLabel: UILabel!
+    
+    @IBOutlet weak var accountTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var legalButton: UIButton!
+    
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var loadingIndicatorView: UIActivityIndicatorView!
+    @IBOutlet weak var findButton: UIButton!
+    @IBOutlet weak var autoLoginLegalButton: UIButton!
+
+    @IBOutlet weak var legalButtonHeightLayoutConstraint: NSLayoutConstraint!
+
+    @IBOutlet weak var autoLoginLegalButtonHeightLayoutConstraint: NSLayoutConstraint!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
+        UserDefaults.standard.removeObject(forKey: DefineStrings.kAgreedAutoLogin)
     }
     
-    override func viewDidAppear(_ animated: Bool) {
-//        self.autoLoginLegalButton.constant = 0
+    override func viewWillLayoutSubviews() {
+        self.loadingIndicatorView.isHidden = true
+        self.updateAutoLoginLegalViews()
+    }
+    
+    func updateAutoLoginLegalViews() {
+        let kButtonHeight: CGFloat = 44
+        let kAnimationDuration = 0.2
+        if let _ = UserDefaults.standard.string(forKey: DefineStrings.kAgreedAutoLogin) {
+            UIView.animate(withDuration: kAnimationDuration) {
+                self.autoLoginLegalButtonHeightLayoutConstraint.constant = kButtonHeight
+                self.legalButtonHeightLayoutConstraint.constant = 0
+                self.view.layoutIfNeeded()
+            }
+            
+            return
+        }
+        
+        let frame = self.legalButton.frame
+        let edge = self.legalButton.imageEdgeInsets
+        self.legalButton.imageEdgeInsets = UIEdgeInsets(top: edge.top, left: frame.width - kButtonHeight, bottom: edge.bottom, right: edge.right)
+
+        UIView.animate(withDuration: kAnimationDuration) {
+            self.autoLoginLegalButtonHeightLayoutConstraint.constant = 0
+            self.legalButtonHeightLayoutConstraint.constant = kButtonHeight
+            self.view.layoutIfNeeded()
+        }
     }
 
+    @IBAction func touchedLegalButton(_ sender: Any) {
+        guard let button = sender as? UIButton else {
+            return
+        }
 
+        button.isSelected = !button.isSelected
+    }
+    
+    @IBAction func touchedLoginButton(_ sender: Any) {
+        guard let account = self.accountTextField.text else {
+            return
+        }
+        
+        guard let password = self.passwordTextField.text else {
+            return
+        }
+
+        self.initializeLoginViews(enabled: false)
+        self.statusLabel.text = NSLocalizedString("logingStatusTitle", comment: "")
+        self.detailLabel.text = ""
+        self.requestLogin(account: account, password: password) { (dict, error) in
+            self.initializeLoginViews(enabled: true)
+            self.statusLabel.text = NSLocalizedString("loginedStatusTitle", comment: "")
+            
+            if self.legalButton.isSelected == true {
+                UserDefaults.standard.setValue(DefineStrings.kAgreedAutoLogin, forKey: DefineStrings.kAgreedAutoLogin)
+            }
+            else {
+                UserDefaults.standard.removeObject(forKey: DefineStrings.kAgreedAutoLogin)
+            }
+            
+            self.updateAutoLoginLegalViews()
+        }
+    }
+    
+    func initializeLoginViews(enabled: Bool) {
+        self.loadingIndicatorView.isHidden = enabled
+        self.accountTextField.isEnabled = enabled
+        self.passwordTextField.isEnabled = enabled
+        self.legalButton.isEnabled = enabled
+        self.loginButton.isEnabled = enabled
+        self.findButton.isEnabled = enabled
+        self.autoLoginLegalButton.isEnabled = enabled
+    }
+
+    func requestLogin(account: String, password: String, completion: @escaping ((NSDictionary?, NSError?) -> Void)) {
+        DispatchQueue.global().asyncAfter(deadline: .now() + 2.0) { [weak self] in
+            
+            DispatchQueue.main.sync {
+                completion(nil, nil)
+            }
+        }
+    }
+    
+    @IBAction func touchedFindButton(_ sender: Any) {
+        
+    }
+    
+    @IBAction func touchedAutoLoginLegalButton(_ sender: Any) {
+        
+    }
+    
 }
 
